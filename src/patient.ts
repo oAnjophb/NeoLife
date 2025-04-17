779b73b9e1697de088053a344c7ba6a9ba204e09
Ruan Anjos <anjosruanp@gmail.com>
2025-04-17 17:16:23 -0300

    Atualizações das classes (Employee, Patient, Receptionist)

--- src/patient.ts
+++ src/patient.ts
@@ -10,24 +10,27 @@ export class Patient {
     public readonly cpf: number
     public readonly age: number
     public readonly id_patient: number
+    public readonly status: StatusType
 
     private constructor(
         name: string,
         cpf: number,
         age: number,
         id_patient: number,
+        status: StatusType
     ) {
         this.name = name
         this.cpf = cpf
         this.age = age
         this.id_patient = id_patient
+        this.status = status
     }
 
-    public static register (name: string, cpf: number, age: number, id_patient: number): Patient{
-        return new Patient(name, cpf, age, id_patient)
+    public static register (name: string, cpf: number, age: number, id_patient: number, status: StatusType): Patient{
+        return new Patient(name, cpf, age, id_patient, status)
     }
 
     public showInfo(): void {
-        console.log (`PACIENTE: ${this.name}\nIDADE: ${this.age}\nID: ${this.id_patient}`)
+        console.log (`PACIENTE: ${this.name}\nIDADE: ${this.age}\nID: ${this.id_patient}\nSTATUS: ${this.status}`)
     }
 }
--- src/receptionist.ts
+++ src/receptionist.ts
@@ -12,7 +12,7 @@ export class Receptionist extends Employee {
     }
 
     registerPatient(name: string, cpf: number, age: number, id_patient: number, StatusType: StatusType): Patient {
-        let paciente = Patient.register(name, cpf, age, id_patient)
+        let paciente = Patient.register(name, cpf, age, id_patient, StatusType)
         this.patientQueue.push(paciente)
         
         return paciente
