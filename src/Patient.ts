1ddc5c4fbf86b38cdfac6411073841794bda953d
Fabricio-Fontenele <vieirafabricio081@gmail.com>
2025-06-11 10:25:37 -0300

    fix: json, db

--- src/Patient.ts
+++ src/Patient.ts
@@ -4,7 +4,7 @@ export class Patient {
   constructor(
     public id: number,
     public name: string,
-    public cpf: string,
+    public cpf: number,
     public age: number,
     public gender: 'M' | 'F',
     public address: string,
